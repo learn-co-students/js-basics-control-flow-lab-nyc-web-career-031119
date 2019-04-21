b056b1185f4aa59c3bb58f5973f1801440604dd9
Caridad Rivera <ccrive10@gmail.com>
2019-04-21 13:45:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,46 +1,60 @@
 // Write your code in this file!
-function scuberGreetingForFeet(num) {
-  if (num <= 400) {
+function scuberGreetingForFeet (feet) {
+  if (feet <= 400) {
     return 'This one is on me!';
-  }
-  else if (num > 2000) {
-    return 'I will gladly take your thirty bucks.'';
-  }
-  else (num > 2500){
-    return 'No can do.'
+  } else if (1999 < feet && feet < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
   }
 }
 
 
-
+// what is the difference between using double quotes and single quotes
+//
 function ternaryCheckCity(city) {
   if (city == 'NYC') {
     return 'Ok, sounds good.'
-  }
-  else  {
+  } else  {
     return 'No go.'
   }
 }
+//
+
 
 // function switchOnCharmFromTip(tip) {
-//   if (tip = 'generous') {
-//     return 'Thank you so much.'
-//   }
-//   else if (tip = 'not as generous') {
-//     return 'Thank you.'
-//   }
-//   else if (tip = 'thanks for everything') {
-//     return 'Bye.'
+//   switch (tip) {
+//     case 'generous':
+//       return 'Thank you so much.';
+//     case 'not as generous.';
+//       return 'Thank you.';
+//     return 'Bye.';
+//
 //   }
 // }
 
-function switchOnCharmFromTip(tip) {
+
+// function scuberGreetingForFeet (feet) {
+//   if (feet <= 400) {
+//     return 'This one is on me!';
+//   } else if (1999 < feet && feet < 2500) {
+//     return "I will gladly take your thirty bucks.";
+//   } else {
+//     return "No can do.";
+//   }
+// }
+//
+// function ternaryCheckCity (city) {
+//   return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+// }
+// //
+function switchOnCharmFromTip (tip) {
   switch (tip) {
     case 'generous':
       return 'Thank you so much.';
-    case 'not as generous.';
+    case 'not as generous':
       return 'Thank you.';
-    return 'Bye.';
-
+    default:
+      return 'Bye.';
   }
 }
