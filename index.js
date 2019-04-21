0a396500eb26c2ae278294f76e012a1326706664
Theo Weller <weller.theodor@gmail.com>
2019-04-21 23:22:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+      return 'This one is on me!'
+  } else if (feet >= 2000 && feet <= 2499) {
+      return 'I will gladly take your thirty bucks.'
+  } else if (feet > 2500) {
+      return 'No can do.'
+    }
+  }
+
+  function ternaryCheckCity(city) {
+    switch (city) {
+      case 'NYC':
+          return 'Ok, sounds good.'
+          break;
+      default:
+          return 'No go.'
+          break;
+      }
+    }
+
+function switchOnCharmFromTip(charm) {
+  switch (charm) {
+    case: 'generous';
+        return 'Thank you so much.';
+        break;
+    case: 'not as generous'
+        return 'Thank you.'
+        break;
+    default:
+      return 'Bye.'
+      break;
+  }
+}
