874b8a9c7e39f081549763bc896b373868c0cd4b
John Lee <jslee1992@gmail.com>
2019-04-21 22:31:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your code in this file!
+function scuberGreetingForFeet(input){
+  let result;
+  if (input < 400){
+    result = "This one is on me!"
+  } else if (input > 2500){
+    result = "No can do."
+  } else if (input > 2000){
+    result = "I will gladly take your thirty bucks."
+  }
+  return result;
+}
+
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go."
+}
+
+let response;
+function switchOnCharmFromTip(input){
+  switch (input) {
+    case "generous":
+      response = "Thank you so much."
+      break;
+    case "not as generous":
+      response = 'Thank you.'
+      break;
+    default:
+      response = 'Bye.'
+      break;
+  }
+  return response;
+}
